7dcf6de2d5e412f1c2b4731bddc9e62b61b313f1
alvian21 <alvian.ardhiansyah@gmail.com>
2025-08-17 13:57:06 +0700

    add connetion database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: process.env.DB_SYNC === 'true',
+      ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
